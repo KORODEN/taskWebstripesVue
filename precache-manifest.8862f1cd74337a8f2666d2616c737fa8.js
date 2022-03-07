5c7f16f0d596985988542146aa7170a273e55a08
KORODEN <47099360+KORODEN@users.noreply.github.com>
2022-03-07 15:20:00 +0700

    Add files via upload

---
+++ precache-manifest.8862f1cd74337a8f2666d2616c737fa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d22d690e4bc4467dd6ea24c64ed4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b34a0035c6de19f7b6",
+    "url": "/js/app.ef846285.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
